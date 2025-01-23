71a7eeb40a2a1cdca2500ef93ee89057207ee31a
Mirpri <71537369+Mirpri@users.noreply.github.com>
2025-01-23 12:21:59 +0800

    Update README.md and better high dpi implementation

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Pong-N
 Pong Neon is a modern twist on the classic arcade game, ***Pong***. With simple controls and addictive gameplay, Pong Neon is easy to pick up and play, but challenging to master. Challenge a friend in 2-player mode for endless hours of fun. Can you beat the high score and become the neon pong champion? [Download](https://github.com/Mirpri/Pong-N/releases/) Pong Neon now and find out!
 ![image](https://github.com/Mirpri/Pong-N/assets/71537369/c72c3cf3-fb53-4393-ac9b-0f6390d2efa6)
+
+## ✨Deep learning with Pong Neon!
+I'm working on a feature that allows players to log their game and load models as opponent. You can log the game data to train a model in your way and load the model to see how it performs!
+
+Stay tuned for updates on this exciting feature, and get ready to take your Pong Neon experience to the next level with AI-powered opponents. Whether you're a machine learning enthusiast or just looking for a new challenge, this feature will add a whole new dimension to the game.
---
+++ legacy/pong-neon-518.py
@@ -0,0 +1,498 @@
+from tkinter import *
+from tkinter.ttk import*
+import keyboard
+import _thread
+import time
+import random
+from subprocess import run
+import sv_ttk
+
+root = Tk()
+root.title('pong neon by Mirpri')
+root.geometry('700x950')
+#root.option_add('*background','#002255')
+#root.option_add('*foreground','ivory')
+#root.config(background='#002255')
+
+sv_ttk.set_theme("light")
+
+p_name=['balanced','large','friction','swift','Sparky','Lightning','Archer' ,'Smokey',     'Wizard'    ,     '(test)','<random>']
+p_width=[40,          55,      30,       27,     30,        35    ,   38    ,   30   ,        34        ,      1000,      30]
+p_fric=[100,         100,      40,       200,    100,      300    ,   110   ,   120  ,         90       ,       100,       100]
+p_speed=[14,          10,       12,      18,     12,        12    ,   13    ,   12   ,        11      ,         0,         0]
+p_color=['gray','tan','darkseagreen','blueviolet','peru','skyblue' ,'orchid', 'rosybrown',  'crimson' ,  'whitesmoke','white']
+p_ac=[    0,          0,        0,      0,        0.4,      0.5,      1 ,      0.7  ,         0.8 ,              0,       0]
+p_ex=[   200,         200,      200,     200,     100,       80,       70,     90    ,        100 ,             200,     200]
+p_cnr=[     1.4,      1.2,      0.9,     1.5,     1.2,       1.2    ,    1   ,  1.1,          0.9 ,              1,      1]
+#p_hit=['pass','pass','pass','pass','sparky()','pass']
+
+def Sparky_app(x):
+    global p1ready,p2ready
+    if x==1:
+        p1ready=1
+        M.itemconfig(p1p,fill='aqua')
+    elif x==2:
+        p2ready=1
+        M.itemconfig(p2p,fill='aqua')
+def Sparky_hit(x):
+    global vy,p1ready,p2ready
+    if x==1:
+        p1ready=0
+        M.itemconfig(p1p,fill='peru')
+    elif x==2:
+        p2ready=0
+        M.itemconfig(p2p,fill='peru')
+    vy*=20
+def lightning(x):
+    global p1stop,p2stop,p1ready,p2ready,p1e,p2e
+    for i in range(2):
+        for j in M.find_all():
+            M.move(j,2,2)
+        time.sleep(1/10)
+        for j in M.find_all():
+            M.move(j,-2,-2)
+        time.sleep(1/10)
+    if x==1:
+        p2stop+=1
+        p2e=max(p2e-25,0)
+        p1ready=1
+        for i in range(3):
+            M.move(p2p,0,10)
+            time.sleep(1/30)            
+            M.move(p2p,0,-10)
+            time.sleep(1/30)
+        p1ready=0
+        p2stop-=1
+    elif x==2:        
+        p1stop+=1
+        p1e=max(p1e-25,0)
+        p2ready=1
+        for i in range(3):
+            M.move(p1p,0,10)
+            time.sleep(1/30)            
+            M.move(p1p,0,-10)
+            time.sleep(1/30)
+        p2ready=0        
+        p1stop-=1
+def Lightning_app(x):
+    _thread.start_new_thread(lightning,(x,))
+def Lightning_hit(x):
+    pass
+def Archer_app(x):
+    _thread.start_new_thread(aiming,(x,))
+def aiming(x):
+    global p1ready,p2ready,p1stop,p2stop,p1a,p2a
+    aim=M.create_text(300,400,text='+',font='Arial 30',fill='orchid')    
+    if x==1:
+        p1a=300
+        p1ready=1        
+        p1stop+=1
+        while gaming and p1ready:
+            if keyboard.is_pressed('a') and not keyboard.is_pressed('d') and p1a>0:
+                p1a-=30
+                M.move(aim,-30,0)
+            elif keyboard.is_pressed('d') and not keyboard.is_pressed('a') and p1a<600:
+                p1a+=30
+                M.move(aim,30,0)
+            if keyboard.is_pressed('w'):
+                p1ready=0
+                break
+            time.sleep(1/30)        
+        p1stop-=1
+    elif x==2:
+        p2a=300
+        p2ready=1        
+        p2stop+=1
+        while gaming and p2ready:
+            if keyboard.is_pressed('4') and not keyboard.is_pressed('6') and p2a>0:
+                p2a-=30
+                M.move(aim,-30,0)
+            elif keyboard.is_pressed('6') and not keyboard.is_pressed('4') and p2a<600:
+                p2a+=30
+                M.move(aim,30,0)
+            if keyboard.is_pressed('8'):
+                p2ready=0
+                break
+            time.sleep(1/30)        
+        p2stop-=1
+    M.delete(aim)
+def Archer_hit(i):
+    global p1ready,p2ready,vx,vy,p1a,p2a,x,y
+    if i==1:
+        p1ready=0        
+        vx,vy=(p1a-x)/((p1a-x)**2+(400-y)**2)**0.5*20,(400-y)/((p1a-x)**2+(400-y)**2)**0.5*max(15,(vx**2+vy**2)**0.5)
+    if i==2:
+        p2ready=0        
+        vx,vy=(p2a-x)/((p2a-x)**2+(400-y)**2)**0.5*20,(400-y)/((p2a-x)**2+(400-y)**2)**0.5*max(15,(vx**2+vy**2)**0.5)
+
+def stamplif(x,y,sz,colorlight='#eeeedd',colordark='gray'):
+    if not dark:
+        s=M.create_oval(x-10*sz,y-10*sz,x+10*sz,y+10*sz,width=0,fill=colorlight)
+    if dark:
+        s=M.create_oval(x-10*sz,y-10*sz,x+10*sz,y+10*sz,width=0,fill=colordark)
+    M.lift(s)
+    for i in range(10):
+        M.coords(s,x-(10-i)*sz,y-(10-i)*sz,x+(10-i)*sz,y+(10-i)*sz)
+        time.sleep(0.1)
+    M.delete(s)
+def smoke(ii):
+    global x,y,p1ready,p2ready
+    if ii==1:
+        p1ready=1
+    elif ii==2:
+        p2ready=1
+    for i in range (5):
+        _thread.start_new_thread(stamplif,(x,y,3*i))
+        time.sleep(0.2)
+    if ii==1:
+        p1ready=0
+    elif ii==2:
+        p2ready=0
+def Smokey_app(i):
+    _thread.start_new_thread(smoke,(i,))
+def Smokey_hit(i):
+    global x,y
+    _thread.start_new_thread(stamplif,(x,y,random.randint(35,50)))
+def Wizard_app(i):
+    global a,x,y
+    _thread.start_new_thread(stamplif,(x,y,2,'crimson','crimson'))
+    a=0.1*(random.randint(0,1)-0.5)*vy
+
+
+#p_effe=['']
+p1t=p2t=0
+def drawpad():    
+    M.coords(p1p,(p1-p_width[p1t],700,p1+p_width[p1t],710))
+    M.coords(p2p,(p2-p_width[p2t],90,p2+p_width[p2t],100))
+    M.itemconfig(p1p,fill=p_color[p1t])
+    M.itemconfig(p2p,fill=p_color[p2t])
+
+def todrawpad(x):
+    global p1t,p2t
+    c1=C1.get()
+    c2=C2.get()
+    
+    p1t=p_name.index(c1)
+    p2t=p_name.index(c2)
+    drawpad()
+
+F1=Frame(root)
+L1=Label(F1,text='  |  '+'⚪'*5)
+C1=Combobox(F1,values=p_name,width=8)
+C1.set(p_name[0])
+C1.bind("<<ComboboxSelected>>",todrawpad)
+P1=Progressbar(root,mode='determinate',length=500)
+
+C1.pack(side='left')
+L1.pack(side='left')
+F2=Frame(root)
+L2=Label(F2,text='  |  '+'⚪'*5)
+C2=Combobox(F2,values=p_name,width=8)
+C2.set(p_name[0])
+C2.bind("<<ComboboxSelected>>",todrawpad)
+P2=Progressbar(root,mode='determinate',length=500)
+
+C2.pack(side='left')
+L2.pack(side='left')
+
+M=Canvas(root,height=800,width=600,bg='white')
+F2.pack(expand=1)
+P2.pack(expand=1)
+M.pack(padx=5)
+P1.pack(expand=1)
+F1.pack(expand=1)
+
+#keyboard.press_and_release('shift')
+
+p1=300
+p2=300
+p1p=M.create_rectangle(0,0,0,0)
+p2p=M.create_rectangle(0,0,0,0)
+ball=M.create_oval(300-10,400-10,300+10,400+10,fill='red',outline='orange',width=0)
+drawpad()
+
+gaming=0
+def match(ii=5,allrandom=0):
+    global gaming
+    gaming=1    
+    s1=s2=0
+    L1.config(text='  |  '+'⚫'*s1+'⚪'*(ii-s1))
+    L2.config(text='  |  '+'⚫'*s2+'⚪'*(ii-s2))
+    c1=C1.get()
+    c2=C2.get()
+    if c1=='<random>':
+        c1=p_name[random.randint(0,len(p_name)-3)]
+        C1.set(c1)
+    if c2=='<random>':
+        c2=p_name[random.randint(0,len(p_name)-3)]
+        C2.set(c2)
+    todrawpad(0)
+
+    while gaming:
+        if allrandom:
+            c1=p_name[random.randint(0,len(p_name)-3)]
+            C1.set(c1)
+            c2=p_name[random.randint(0,len(p_name)-3)]
+            C2.set(c2)            
+            todrawpad(0)
+        win=game()
+        if not gaming:
+            break
+        if win==1:
+            s1+=1        
+        else:
+            s2+=1
+        if s1==s2 and s1==ii-1 and ii<10:
+            ii+=1
+        if gaming:
+            presentstar(win)
+        L1.config(text='  |  '+'⚫'*s1+'⚪'*(ii-s1))
+        L2.config(text='  |  '+'⚫'*s2+'⚪'*(ii-s2))
+        if s1==ii or s2==ii:
+            gaming=0
+    if s1==ii:
+        presentstar(1,ii)
+    elif s2==ii:
+        presentstar(2,ii)
+
+    F3.pack(pady=5)      
+    B1.config(state=NORMAL)
+    C1.config(state=NORMAL)
+    C2.config(state=NORMAL)
+    return
+
+def stamp(x,y):
+    if not dark:
+        s=M.create_oval(x-10,y-10,x+10,y+10,width=0,fill='pink')
+    if dark:
+        s=M.create_oval(x-10,y-10,x+10,y+10,width=0,fill='brown')
+    M.lower(s)
+    for i in range(10):
+        M.coords(s,x-(10-i),y-(10-i),x+(10-i),y+(10-i))
+        time.sleep(0.1)
+    M.delete(s)
+
+
+def presentstar(p,num=1):
+    if p==1:
+        color=p_color[p1t]
+    else:
+        color=p_color[p2t]
+    coin=M.create_text(300,400,text="✨"*num,fill=color,font='Arial 1')
+    for i in range(10):
+        M.itemconfig(coin,font='Arial '+str(int(i*50/(num+4))))
+        time.sleep(0.03)
+    for i in range(20):
+        M.move(coin,0,50*(1.5-p))
+        time.sleep(0.03)
+    M.delete(coin)
+
+def game():
+    global x,y,vx,vy,a,p1e,p2e,p1ready,p2ready,p1v,p2v,p1stop,p2stop
+    p1=300
+    p2=300
+    p1e,p2e,p1ready,p2ready,p1stop,p2stop=0,0,0,0,0,0
+    p1v,p2v=p_speed[p1t],p_speed[p2t]
+    x,y=300,400
+    a=0
+    M.coords(ball,(x-10,y-10,x+10,y+10))
+    drawpad()
+    vx,vy=5*(2*random.randint(0,1)-1),6*(2*random.randint(0,1)-1)
+    #vx=0
+    root.update()
+    #keyboard.wait('space')
+    while gaming:
+        if keyboard.is_pressed('space'):
+            break
+        time.sleep(0.02)
+    boring=0
+    while gaming:
+        s_t=time.time()
+        v1,v2=0,0
+        x_c,y_c=x,y
+        vx+=a
+
+        if p1e<100:
+            p1e+=p_ac[p1t]
+            P1['value']=int(p1e)
+        if p2e<100:
+            p2e+=p_ac[p2t]
+            P2['value']=int(p2e)
+
+
+        if keyboard.is_pressed('a') and not p1stop and p1>p_width[p1t]:
+            p1-=p1v
+            v1-=1
+            M.move(p1p,-p1v,0)
+        if keyboard.is_pressed('d') and not p1stop and p1<600-p_width[p1t]:
+            p1+=p1v
+            v1+=1
+            M.move(p1p,p1v,0)
+        if keyboard.is_pressed('4') and not p2stop and p2>p_width[p2t]:
+            p2-=p2v
+            v2-=1
+            M.move(p2p,-p2v,0)
+        if keyboard.is_pressed('6') and not p2stop and p2<600-p_width[p2t]:
+            p2+=p2v
+            v2+=1
+            M.move(p2p,p2v,0)
+        if keyboard.is_pressed('s') and p1e>=p_ex[p1t] and not p1ready:
+            keyboard.release('s')
+            p1e-=p_ex[p1t]
+            exec(p_name[p1t]+'_app(1)')
+        if keyboard.is_pressed('2') and p2e>=p_ex[p2t] and not p2ready:
+            keyboard.release('2')
+            p2e-=p_ex[p2t]
+            exec(p_name[p2t]+'_app(2)')
+
+        x,y=x+vx,y+vy
+        if x<10:
+            vx=-vx
+            x=10
+        elif x>590:
+            vx=-vx
+            x=590
+
+        if min(80,110+vy)<y<110 and abs(p2-x)<p_width[p2t]+10:
+            if y-vy<80 :#and abs(x-p2)>p_width[p2t]+5:
+                if x>p2:
+                    vx=abs(vx)
+                else:
+                    vx=-abs(vx)
+                vy*=p_cnr[p2t]
+                vx*=p_cnr[p2t]
+                print('p2^')
+            vy=-vy+1
+            vx+=v2*p2v/4
+            a=(v2*p2v-vx)/p_fric[p2t]
+            y=110
+            if p2ready:
+                exec(p_name[p2t]+'_hit(2)')
+            #print(vy)
+        elif max(720,690+vy)>y>690 and abs(p1-x)<p_width[p1t]+10:
+            if y-vy>690 :#and abs(x-p1)>p_width[p1t]+5:
+                if x>p1:
+                    vx=abs(vx)
+                else:
+                    vx=-abs(vx)
+                vy*=p_cnr[p1t]
+                vx*=p_cnr[p1t]
+                print('p1^')
+            vy=-vy-1
+            vx+=v1*p1v/4
+            a=(v1*p1v-vx)/p_fric[p1t]
+            y=690            
+            if p1ready:
+                exec(p_name[p1t]+'_hit(1)')
+            #print(vy)
+        M.move(ball,x-x_c,y-y_c)
+        if random.randint(0,1)==0:
+            _thread.start_new_thread(stamp,(x,y))
+
+        if abs(vx)<abs(0.3*vy):
+            boring+=1
+            if boring>250:
+                M.itemconfig(ball,width=int((boring-250)/5))
+                if boring>300:
+                    #vx=random.randint(int(abs(vy)),int(abs(vy*2)))*(random.randint(0,1)-0.5)
+                    a=0.2*(random.randint(0,1)-0.5)*vy
+                    boring=0
+                    M.itemconfig(ball,width=0)
+            #print(boring)
+        else:
+            boring=0
+            M.itemconfig(ball,width=0)
+        if vx>40:
+            vx-=0.2
+        if vx<-40:
+            vx+=0.2
+        if vy>40:
+            vy=40
+            print(vy)
+        elif vy<-40:
+            vy=-40
+            print(vy)
+        elif vy>25:
+            vy-=0.2
+        elif vy<-25:
+            vy+=0.2
+        if y<0:
+            time.sleep(0.1)
+            p1ready=p2ready=p1stop=p2stop=0
+            return 1
+        elif y>800:
+            time.sleep(0.1)
+            p1ready=p2ready=p1stop=p2stop=0
+            return 2
+
+        #root.update()
+        #print(0.02-time.time()+s_t)
+        sleep_t=(1/30)-time.time()+s_t
+        if sleep_t>0:
+            time.sleep(sleep_t)
+        else:
+            print(sleep_t,'!')
+
+started=False
+def startmatch(event=0):
+    global gaming,started
+    if not started:       
+        keyboard.press_and_release('shift')
+        started=True
+    if gaming:
+        return
+    C1.set(p_name[p1t])
+    C2.set(p_name[p2t])
+    F3.forget()
+    B1.config(state=DISABLED)
+    C1.config(state=DISABLED)
+    C2.config(state=DISABLED)
+    B1.focus_set()
+    if ch1.get():
+        _thread.start_new_thread(match,(3,1))
+    else:
+        _thread.start_new_thread(match,())
+
+
+def opengp():
+    run("start D:\\vscodes\\GP", shell=True)
+    time.sleep(0.1)
+    keyboard.write('np666')
+    keyboard.press_and_release('enter')
+dark=0
+def themealt():
+    global dark
+    if dark:
+        sv_ttk.set_theme('light')
+        M.config(bg='white')
+        M.itemconfig(ball,fill='red')
+        dark=0
+    else:
+        sv_ttk.set_theme('dark')
+        M.config(bg='#333333')
+        M.itemconfig(ball,fill='#FF8888')
+        dark=1
+ch1=IntVar()
+F3=Frame(root)
+F3.pack(pady=5)
+B1=Button(F3,text='Start',width=20,command=startmatch)
+B1.pack(side='left',padx=2)
+Ch1=Checkbutton(F3,text='All-Random',variable=ch1,onvalue=1,offvalue=0,width=15)
+Ch1.pack(side='left',padx=2)
+B2=Button(F3,text='GP',command=opengp,width=3)
+B2.pack(side='left',padx=2)
+B3=Button(F3,text='🔆',command=themealt,width=3)
+B3.pack(padx=2)
+#keyboard.is_pressed('a')
+#_thread.start_new_thread(game,())
+
+def forcestop(x):
+    global gaming
+    gaming=0
+
+
+
+
+root.bind('<Escape>',forcestop)
+root.bind('<Return>',startmatch)
+root.mainloop()
